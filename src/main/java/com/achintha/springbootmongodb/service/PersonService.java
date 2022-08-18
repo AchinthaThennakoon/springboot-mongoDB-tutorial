1395c16b35a4b24ff58e815f0c054cea5a7b14d6
achintha <achinthathennakoon97@gmail.com>
2022-08-18 10:54:02 +0530

    Added save person method

--- src/main/java/com/achintha/springbootmongodb/service/PersonService.java
+++ src/main/java/com/achintha/springbootmongodb/service/PersonService.java
@@ -1,4 +1,8 @@
 package com.achintha.springbootmongodb.service;
 
+import com.achintha.springbootmongodb.collection.Person;
+
 public interface PersonService {
+
+    public String save(Person person);
 }
--- src/main/java/com/achintha/springbootmongodb/service/impl/PersonServiceImpl.java
+++ src/main/java/com/achintha/springbootmongodb/service/impl/PersonServiceImpl.java
@@ -1,8 +1,20 @@
 package com.achintha.springbootmongodb.service.impl;
 
+import com.achintha.springbootmongodb.collection.Person;
+import com.achintha.springbootmongodb.repository.PersonRepository;
 import com.achintha.springbootmongodb.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PersonServiceImpl implements PersonService {
+
+    @Autowired
+    PersonRepository personRepository;
+
+    @Override
+    public String save(Person person) {
+        return personRepository.save(person).getId();
+    }
+
 }
