4b7979c938e3eea887a17b00e279af9995d9e6de
achintha <achinthathennakoon97@gmail.com>
2022-08-18 10:33:26 +0530

    created person repo

---
+++ src/main/java/com/achintha/springbootmongodb/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.achintha.springbootmongodb.repository;
+
+import com.achintha.springbootmongodb.collection.Person;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends MongoRepository<Person,String> {
+}
