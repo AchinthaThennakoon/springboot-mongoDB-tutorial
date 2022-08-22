657f193186c22e656b0a953ca2fd269dcea07688
achintha <achinthathennakoon97@gmail.com>
2022-08-22 08:06:04 +0530

    created method findByFirstNameStartsWith

--- src/main/java/com/achintha/springbootmongodb/repository/PersonRepository.java
+++ src/main/java/com/achintha/springbootmongodb/repository/PersonRepository.java
@@ -4,6 +4,11 @@ import com.achintha.springbootmongodb.collection.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PersonRepository extends MongoRepository<Person,String> {
+    //define the implementation
+    List<Person> findByFirstNameStartsWith(String name);
+
 }
